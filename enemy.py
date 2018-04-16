5cef0d208ba8f82d025de7f3e33ac29cc8b0c24a
forscienceandpie <38381721+forscienceandpie@users.noreply.github.com>
2018-04-17 00:19:58 +0300

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -4,9 +4,24 @@ class enemy():
         self.stren = stren
         self.intel = intel
         self.agil = agil
-        self. atten = atten
-        self. charis = charis
-    
+        self.atten = atten
+        self.charis = charis
+        
+    def change_stren(self, stren, change):
+        self.stren += change;
+        
+    def change_intel(self, intel, change):
+        self.intel += change;
+        
+    def change_agil(self, agil, change):
+        self.agil += change
+        
+    def change_atten(self, atten, change):
+        self.atten += change
+
+    def change_charis(self, charis,change):
+        self.charis += change
+      
 class sphinx(enemy):
     def __init__(self, stren, intel, agil, atten, charis):
         super().__init__(stren, intel, agil, atten, charis)
